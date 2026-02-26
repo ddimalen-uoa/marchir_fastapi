38dc66338f320ffd8aec582173e0a0dda8a5f866
ddimalen-uoa <d.dimalen@auckland.ac.nz>
2026-02-26 23:04:48 +1300

    d-branch-8
    d-branch-8

--- backend/config_validator.py
+++ backend/config_validator.py
@@ -1,13 +1,16 @@
 import v1.uploaded.project_validators as project_validators
 from v1.uploaded.project_validators import *
 
+# Dictionary of Validation Functions
 validator_functions = {
     'form_validator': project_validators.form_validator.run_validator,
     'selector_validator': project_validators.selector_validator.run_validator    
 }
 
+# Form validator config
 form_id = "form-ct"
 
+# Format is {section_id: num_fields, ...}
 form_sections = { 
     "user-details":4, 
     "address-details":2, 
